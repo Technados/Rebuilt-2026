9ed727befbc02a5f34bb24e6bb9ae7340e801b45
Technados <117394920+Technados@users.noreply.github.com>
2026-01-14 16:27:42 -0500

    Updated vendor dependencies
    Replaced Studica with NavX, updated Configs to match the json files

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.FeedbackSensor;
 //import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
