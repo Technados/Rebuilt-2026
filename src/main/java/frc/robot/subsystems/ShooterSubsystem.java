90354ef8a60ba98ed33544647695a7511ccf873b
Technados <117394920+Technados@users.noreply.github.com>
2026-02-20 10:44:39 -0500

    Updated ShooterSubsystem, IntakeSubsyste,
    Added motor encoders and shooterAtSpeed method to ShooterSubsystem. Added comments to IntakeSubsystem.
    
    Co-Authored-By: em-g73 <190783012+em-g73@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,11 +5,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
@@ -18,8 +20,12 @@ import frc.robot.Constants.ShooterConstants;
 public class ShooterSubsystem extends SubsystemBase {
 
   private final SparkMax preShooterMotor;
+
   private final SparkFlex leftShooterMotor;
+  private final RelativeEncoder leftShooterEncoder;
+
   private final SparkFlex rightShooterMotor;
+  private final RelativeEncoder rightShooterEncoder;
 
   public ShooterSubsystem() {
     // Initializes motors using constants and configs
@@ -39,34 +45,46 @@ public class ShooterSubsystem extends SubsystemBase {
       Configs.ShooterSubsystem.rightShooterConfig,
       ResetMode.kResetSafeParameters,                                                   
       PersistMode.kPersistParameters);
+
+    leftShooterEncoder = leftShooterMotor.getEncoder();
+    rightShooterEncoder = rightShooterMotor.getEncoder();
   }
 
-  public void setPower(SparkFlex motor, double power) { // Sets the power of the given SparkFlex motor
-    motor.set(power);
+  public boolean shooterAtSpeed() { // Returns true if the motors are at full speed, doesn't have the right constants yet
+    return 
+      (leftShooterEncoder.getVelocity() == ShooterConstants.kLeftShooterMotorPower) &&
+      (rightShooterEncoder.getVelocity() == ShooterConstants.kRightShooterMotorPower);
+      
   }
 
   public Command runShooterCommand() { // Runs the main shooter
     return this.startEnd(
       () -> {
         preShooterMotor.set(ShooterConstants.kPreShooterMotorPower);
-        setPower(leftShooterMotor, ShooterConstants.kLeftShooterMotorPower);
-        setPower(rightShooterMotor, ShooterConstants.kRightShooterMotorPower);
+        leftShooterMotor.set(ShooterConstants.kLeftShooterMotorPower);
+        rightShooterMotor.set(ShooterConstants.kRightShooterMotorPower);
       },
       () -> {
         preShooterMotor.set(0.0);
-        setPower(leftShooterMotor, 0.0);
-        setPower(rightShooterMotor, 0.0);
+        leftShooterMotor.set(0.0);
+        rightShooterMotor.set(0.0);
       }
     );
   }
 
-  public Command idleShooterCommand() { // Runs the main shooter at 20% power
+  public Command idleShooterCommand() { // Runs the main shooter at idle power
     return this.run(
       () -> {
-        setPower(leftShooterMotor, ShooterConstants.kLeftShooterIdlePower);
-        setPower(rightShooterMotor, ShooterConstants.kRightShooterIdlePower);
+        leftShooterMotor.set(ShooterConstants.kLeftShooterIdlePower);
+        rightShooterMotor.set(ShooterConstants.kRightShooterIdlePower);
       }
     );
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Left Velocity", leftShooterEncoder.getVelocity());
+    SmartDashboard.putNumber("Right Velocity", rightShooterEncoder.getVelocity());
   }
 
 }
